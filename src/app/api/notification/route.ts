76d580875f0be1984107936e540606fb13ecec1d
DiogoAlberto0 <dafgo03@gmail.com>
2024-10-25 16:41:16 -0300

    add notification router

--- src/app/api/notification/route.ts
+++ src/app/api/notification/route.ts
@@ -1,19 +1,12 @@
-import { MercadoPagoConfig, Preference } from "mercadopago";
-import { type NextRequest } from 'next/server'
 
-export async function POST(req: NextRequest) {
 
-    const client = new MercadoPagoConfig({ accessToken: process.env.ACCESS_TOKEN_TEST || '' })
+export async function POST(req: Request) {
 
-    const mercadopago = new Preference(client)
 
     const res = await req.json()
     const { type, data } = res;
 
-    const preference = mercadopago.get({ preferenceId: data.id })
-
     console.log({
-        preference,
         data,
         type
     })
